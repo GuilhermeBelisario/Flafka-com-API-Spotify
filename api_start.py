62cad90ad776f8534ab2d300d43c4a403e4f5e2d
Guilherme Belisario <gbo2000@outlook.com>
2024-06-20 21:10:34 -0300

    Criação do Loop de leitura do arquivo e o de consultas

--- api_start.py
+++ api_start.py
@@ -1,4 +1,5 @@
 import spotipy
+import time
 from spotipy.oauth2 import SpotifyClientCredentials
 
 # Substitua pelos seus próprios Client ID e Client Secret
@@ -9,27 +10,34 @@ client_secret = '5a7c494baa0c4946b86da0e4a619715b'
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-#Criando o coomeço dos arquivos ordenados sobre artista/genero
-# que serão salvos como json
 class ArtistSP:
+    #Criando o começo dos arquivos ordenados sobre artista/genero
+    # que serão salvos como json
     def __init__(self, artist_name):
         self.artist_name = artist_name
 
     def __str__ (self):
         return f'{self.artist_name}'
+
+bandas = open('bandas.txt', mode='r', encoding='utf-8' )
+bandas = list(bandas)
+bandas_sem_espaço= [elemento.strip() for elemento in bandas]
+
+for banda_nome in bandas_sem_espaço:
+    band = ArtistSP(banda_nome)
+    result = sp.search(q=band, type='artist')
+    artist = result['artists']['items'][0]
+    # Obtendo as top tracks do artista
+    artist_id = artist['id']
+    artist_genres = artist['genres']
+    top_tracks = sp.artist_top_tracks(artist_id)
+
+    #Mostrando os valores o ranking final
+    print(band)
+    print(artist_genres)
+
+    for idc, track in enumerate(top_tracks['tracks']):
+        print(f"{idc + 1}. {track['name']} - {track['popularity']}")
     
-band = ArtistSP('Bring Me The Horizon')
-result = sp.search(q=band, type='artist')
-artist = result['artists']['items'][0]
-
-# Obtendo as top tracks do artista
-artist_id = artist['id']
-artist_genres = artist['genres']
-top_tracks = sp.artist_top_tracks(artist_id)
-
-#Mostrando os valores o ranking final
-print(band)
-print(artist_genres)
-
-for idc, track in enumerate(top_tracks['tracks']):
-    print(f"{idc + 1}. {track['name']} - {track['popularity']}")
+    time.sleep(5)
+    
