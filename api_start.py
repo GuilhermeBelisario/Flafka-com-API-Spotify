b00a9ca89287916d7d2d3fde289ee3ae8bbe6da2
Guilherme Belisario <gbo2000@outlook.com>
2024-06-22 23:36:55 -0300

    Adicionando a forma de salvar o arquivo

--- api_start.py
+++ api_start.py
@@ -1,6 +1,8 @@
 import spotipy
 import time
+import json
 from spotipy.oauth2 import SpotifyClientCredentials
+from classificar import classificar
 
 # Substitua pelos seus próprios Client ID e Client Secret
 client_id = 'aee7093a67304079a515f10a05bca257'
@@ -19,11 +21,12 @@ class ArtistSP:
     def __str__ (self):
         return f'{self.artist_name}'
 
+#Abrindo o Arquivo TXT e realizando os reparos na estrutura da lista.
 bandas = open('bandas.txt', mode='r', encoding='utf-8' )
 bandas = list(bandas)
-bandas_sem_espaço= [elemento.strip() for elemento in bandas]
+bandas_sem_pular_linha= [elemento.strip() for elemento in bandas]
 
-for banda_nome in bandas_sem_espaço:
+for banda_nome in bandas_sem_pular_linha:
     band = ArtistSP(banda_nome)
     result = sp.search(q=band, type='artist')
     artist = result['artists']['items'][0]
@@ -31,13 +34,20 @@ for banda_nome in bandas_sem_espaço:
     artist_id = artist['id']
     artist_genres = artist['genres']
     top_tracks = sp.artist_top_tracks(artist_id)
-
-    #Mostrando os valores o ranking final
-    print(band)
-    print(artist_genres)
-
+    tracks = []
+    #Mostrando os valores e o ranking final
     for idc, track in enumerate(top_tracks['tracks']):
-        print(f"{idc + 1}. {track['name']} - {track['popularity']}")
-    
-    time.sleep(5)
+        tracks.append(f"{idc + 1}. {track['name']} - {track['popularity']}")
+
+    #Definindo o formato que quero seja salvo o arquivo
+    informacao_adquirida = {'Nome': banda_nome , 'Genero': artist_genres , 'TopSong': tracks} 
+    genero = informacao_adquirida['Genero'][1]
+    nome = informacao_adquirida['Nome']
+
+    #Definindo o caminho do arquivo e nome para cada arquivo
+    banda_arquivo = 'bandas/' + str(genero.capitalize()) + " - " + str(nome.capitalize())
+    #Salvando o JSON
+    with open(banda_arquivo, 'w', encoding='utf-8') as json_file:
+        json_file = json.dump(informacao_adquirida, json_file,ensure_ascii=False,indent=4)
+    time.sleep(2)
     
